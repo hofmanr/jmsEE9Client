a5494d4537851f9b5b9a531df2f7e6c32944d35a
Rinus Hofman <rhofman14@gmail.com>
2023-02-09 23:10:47 +0100

    Upgraded beans.xml and web.xml. Changed the port to 9082 in dbServices.ts

--- src/main/ts/frontend/src/services/dbServices.ts
+++ src/main/ts/frontend/src/services/dbServices.ts
@@ -1,13 +1,13 @@
 import { Queue, Message, Payload } from '../common/types';
 
 async function fetchQueues() {
-    return await fetch('http://localhost:8080/jmsRestClient/api/queues')
+    return await fetch('http://localhost:9082/jmsRestClient/api/queues')
     .then((res: Response) => res.json())
     .then((data: Queue[]) => data);
 }
 
 async function fetchMessages(queue: Queue): Promise<Message[]> {
-    const response = await fetch(`http://localhost:8080/jmsRestClient/api/queues/${queue?.name}/messages`);
+    const response = await fetch(`http://localhost:9082/jmsRestClient/api/queues/${queue?.name}/messages`);
     if (response.status === 204) {
         return new Promise(resolve => resolve([]));
     }
@@ -20,7 +20,7 @@ async function fetchMessages(queue: Queue): Promise<Message[]> {
 }
 
 async function deleteMessage(queue: Queue, message: Message) {
-    return await fetch(`http://localhost:8080/jmsRestClient/api/queues/${queue?.name}/messages/${message.messageID}`,
+    return await fetch(`http://localhost:9082/jmsRestClient/api/queues/${queue?.name}/messages/${message.messageID}`,
     {
         method: "DELETE"
     })
@@ -29,13 +29,13 @@ async function deleteMessage(queue: Queue, message: Message) {
 }
 
 async function fetchPayload(queue: Queue, message: Message) {
-    return await fetch(`http://localhost:8080/jmsRestClient/api/queues/${queue?.name}/messages/${message.messageID}`)
+    return await fetch(`http://localhost:9082/jmsRestClient/api/queues/${queue?.name}/messages/${message.messageID}`)
     .then((res: Response) => res.json())
     .then((data: Payload) => data);
 }
 
 async function postPayload(queue: Queue, payload: string): Promise<string> {
-    const response = await fetch(`http://localhost:8080/jmsRestClient/api/queues/${queue?.name}/messages`,
+    const response = await fetch(`http://localhost:9082/jmsRestClient/api/queues/${queue?.name}/messages`,
     {
         headers: {
           'Accept': 'application/json',
